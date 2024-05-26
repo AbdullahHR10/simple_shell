e25f136fd9c643013b50b66bfd6386949d62911b
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-05-25 20:52:21 -0700

    Include libs

---
+++ shell.h
@@ -0,0 +1,20 @@
+#ifndef SHELL_HEADER
+#define SHELL_HEADER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <<ctype.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+void display_prompt();
+
+#endif
